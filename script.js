34cdb6ddc60d8c944b1c6dc429a316b8423d96d5
Gabe-A001 <gabrielakinola222@gmail.com>
2026-06-30 11:25:03 +0100

    Adding a map and marker

--- script.js
+++ script.js
@@ -5,3 +5,9 @@ let totalNum=num1 + num2
 let text="i have "+ totalNum + " alerts"
     alert(text)
 }
+var map = L.map('map').setView([51.574349, -1.310892], 16);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+var marker = L.marker([51.574349, -1.310892]).addTo(map);
